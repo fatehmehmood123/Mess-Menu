504ad5ec3dd6279e905ddd2a2023e7b57de6ce4e
abdurafeyf <abdurafeyf@gmail.com>
2023-03-02 23:49:42 +0500

    added button to links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,30 +4,33 @@ import logo from "../images/android-chrome-192x192.png"
 export default function Navbar() {
   return (
     <>
-       <nav className="nav navbar navbar-expand-lg bg-body-tertiary">
-    <div className="container-fluid">
-      <Link to="/">
+      <nav className="nav navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <Link to="/">
 
-        <img src={logo} alt="logo" height="70px" width="70px"/>
-      </Link>
-      <Link className="navbar-brand" to="/">Nust Mess Menu</Link>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            
-            <Link className="nav-link active" aria-current="page" to="/">Today</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to="/weekly">This Week</Link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+            <img src={logo} alt="logo" height="70px" width="70px" />
+          </Link>
+          <Link className="navbar-brand" to="/">Nust Mess Menu</Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Button>
+                  <Link className="nav-link active" aria-current="page" to="/">Today</Link>
+                </Button>
+              </li>
+              <li className="nav-item">
+                <Button>
+                  <Link className="nav-link active" aria-current="page" to="/weekly">This Week</Link>
+                </Button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
   )
 }
