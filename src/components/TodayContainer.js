657bcb58fad2b834da30d90c589d4f3005ff494f
abdurafeyf <abdurafeyf@gmail.com>
2023-03-22 23:43:45 +0500

    bold tag simple timings

--- src/components/TodayContainer.js
+++ src/components/TodayContainer.js
@@ -9,21 +9,21 @@ export default function TodayContainer(props) {
         <ol className="list-group list-group-numbered">
           <li className="list-group-item d-flex justify-content-between align-items-start">
             <div className="ms-2 me-auto">
-              <div className="fw-bold">Sehri(2:30 Onwards)</div>
+              <div><b>Sehri</b> (2:30 Onwards)</div>
               {props.breakfast}
             </div>
 
           </li>
           <li className="list-group-item d-flex justify-content-between align-items-start">
             <div className="ms-2 me-auto">
-              <div className="fw-bold">Iftari(Sunset)</div>
+              <div><b>Iftari</b> (Sunset)</div>
               {props.lunch}
             </div>
 
           </li>
           <li className="list-group-item d-flex justify-content-between align-items-start">
             <div className="ms-2 me-auto">
-              <div className="fw-bold">Dinner(7:30-8:30)</div>
+              <div><b>Dinner</b> (7:30 - 8:30)</div>
               {props.dinner}
             </div>
           </li>
