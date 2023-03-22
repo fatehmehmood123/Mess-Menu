1ba9e80150d5b439ef63686561cb33ff647e1d1e
abdurafeyf <abdurafeyf@gmail.com>
2023-03-23 00:33:58 +0500

    hello

--- src/components/TodayContainer.js
+++ src/components/TodayContainer.js
@@ -23,7 +23,7 @@ export default function TodayContainer(props) {
           </li>
           <li className="list-group-item d-flex justify-content-between align-items-start">
             <div className="ms-2 me-auto">
-              <div><b>رات کا کھانا</b> (7:30 - 8:30)</div>
+              <div>(7:30 - 8:30) <b>رات کا کھانا</b></div>
               {props.dinner}
             </div>
           </li>
