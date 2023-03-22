7fb9e2ab6a5d9e77847e627ff370c3d98f3de759
abdurafeyf <abdurafeyf@gmail.com>
2023-03-23 01:04:36 +0500

    ramadan

--- src/components/TodayContainer.js
+++ src/components/TodayContainer.js
@@ -6,26 +6,28 @@ export default function TodayContainer(props) {
       {/* returning component */}
       <div className="container my-4" id="mainContainer">
         <div>
-          رمضان المبارک
+          <h3>
+            رمضان المبارک
+          </h3>
         </div>
         <ol className="list-group list-group-numbered">
           <li className="list-group-item d-flex justify-content-between align-items-start">
             <div className="ms-2 me-auto">
-              <div><b style={{fontSize: "18px"}}>سحری</b> (2:45 Onwards)</div>
+              <div><b style={{ fontSize: "18px" }}>سحری</b> (2:45 Onwards)</div>
               {props.breakfast}
             </div>
 
           </li>
           <li className="list-group-item d-flex justify-content-between align-items-start">
             <div className="ms-2 me-auto">
-              <div><b style={{fontSize: "18px"}}>افطاری</b> (Sunset)</div>
+              <div><b style={{ fontSize: "18px" }}>افطاری</b> (Sunset)</div>
               {props.lunch}
             </div>
 
           </li>
           <li className="list-group-item d-flex justify-content-between align-items-start">
             <div className="ms-2 me-auto">
-              <div><b style={{fontSize: "18px"}}>رات کا کھانا</b> (7:30 to 8:30)</div>
+              <div><b style={{ fontSize: "18px" }}>رات کا کھانا</b> (7:30 to 8:30)</div>
               {props.dinner}
             </div>
           </li>
