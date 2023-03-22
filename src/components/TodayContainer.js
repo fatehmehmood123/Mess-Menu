b0d6df373b742a345b62d27aae92c41817167dae
abdurafeyf <abdurafeyf@gmail.com>
2023-03-22 23:35:24 +0500

    added timing of iftari sehri dinner

--- src/components/TodayContainer.js
+++ src/components/TodayContainer.js
@@ -9,21 +9,21 @@ export default function TodayContainer(props) {
         <ol className="list-group list-group-numbered">
           <li className="list-group-item d-flex justify-content-between align-items-start">
             <div className="ms-2 me-auto">
-              <div className="fw-bold">Sehri</div>
+              <div className="fw-bold">Sehri (2:30 Onwards)</div>
               {props.breakfast}
             </div>
 
           </li>
           <li className="list-group-item d-flex justify-content-between align-items-start">
             <div className="ms-2 me-auto">
-              <div className="fw-bold">Iftari</div>
+              <div className="fw-bold">Iftari (Sunset)</div>
               {props.lunch}
             </div>
 
           </li>
           <li className="list-group-item d-flex justify-content-between align-items-start">
             <div className="ms-2 me-auto">
-              <div className="fw-bold">Dinner</div>
+              <div className="fw-bold">Dinner (7:30 - 8:30)</div>
               {props.dinner}
             </div>
           </li>
