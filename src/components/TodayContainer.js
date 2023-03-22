e4eb8e036f59b1bc9153851cf2a5520e85bfe5ae
abdurafeyf <abdurafeyf@gmail.com>
2023-03-22 23:40:47 +0500

    spacing

--- src/components/TodayContainer.js
+++ src/components/TodayContainer.js
@@ -23,7 +23,7 @@ export default function TodayContainer(props) {
           </li>
           <li className="list-group-item d-flex justify-content-between align-items-start">
             <div className="ms-2 me-auto">
-              <div className="fw-bold">Dinner(7:30 - 8:30)</div>
+              <div className="fw-bold">Dinner(7:30-8:30)</div>
               {props.dinner}
             </div>
           </li>
