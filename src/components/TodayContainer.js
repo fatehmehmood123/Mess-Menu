57ab2ac570e6b738023af7999a2b9b7dca20e3d9
abdurafeyf <abdurafeyf@gmail.com>
2023-03-23 00:32:31 +0500

    urdu

--- src/components/TodayContainer.js
+++ src/components/TodayContainer.js
@@ -9,21 +9,21 @@ export default function TodayContainer(props) {
         <ol className="list-group list-group-numbered">
           <li className="list-group-item d-flex justify-content-between align-items-start">
             <div className="ms-2 me-auto">
-              <div><b>Sehri</b> (2:30 Onwards)</div>
+              <div><b>سحری</b> (2:30 Onwards)</div>
               {props.breakfast}
             </div>
 
           </li>
           <li className="list-group-item d-flex justify-content-between align-items-start">
             <div className="ms-2 me-auto">
-              <div><b>Iftari</b> (Sunset)</div>
+              <div><b>افطاری</b> (Sunset)</div>
               {props.lunch}
             </div>
 
           </li>
           <li className="list-group-item d-flex justify-content-between align-items-start">
             <div className="ms-2 me-auto">
-              <div><b>Dinner</b> (7:30 - 8:30)</div>
+              <div><b>رات کا کھانا</b> (7:30 - 8:30)</div>
               {props.dinner}
             </div>
           </li>
