0e374ba938a20ed7913bb59c3e0023de256fdf26
abdurafeyf <abdurafeyf@gmail.com>
2023-03-22 23:33:36 +0500

    updated todayContainer to new ramadan tags

--- src/components/TodayContainer.js
+++ src/components/TodayContainer.js
@@ -3,20 +3,20 @@ import React from 'react'
 export default function TodayContainer(props) {
   return (
     <>
-    {/* returning component */}
+      {/* returning component */}
       <div className="container my-4" id="mainContainer">
 
         <ol className="list-group list-group-numbered">
           <li className="list-group-item d-flex justify-content-between align-items-start">
             <div className="ms-2 me-auto">
-              <div className="fw-bold">BreakFast</div>
+              <div className="fw-bold">Sehri</div>
               {props.breakfast}
             </div>
 
           </li>
           <li className="list-group-item d-flex justify-content-between align-items-start">
             <div className="ms-2 me-auto">
-              <div className="fw-bold">Lunch</div>
+              <div className="fw-bold">Iftari</div>
               {props.lunch}
             </div>
 
