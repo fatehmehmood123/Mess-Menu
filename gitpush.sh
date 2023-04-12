4e8258ff3b6504926abc84ba3efc2bcdb1b5a2c8
Muhammad Fateh Mehmood <fatehmehmood123@gmail.com>
2023-04-12 20:22:57 +0500

    Automated commit message

--- gitpush.sh
+++ gitpush.sh
@@ -1,5 +1,18 @@
-commit_message="$1"
+
+if [ -z "$1" ]
+then
+    commit_message="Automated commit message"
+else
+    commit_message="$1"
+fi
 
 git add .
 git commit -m "$commit_message"
 git push
+
+
+# following command will make this file executable
+# icacls .\gitpush.sh /grant:r "%USERNAME%:(RX)"
+# to run this enter
+# ./gitpush.sh                  # Uses default commit message
+# ./gitpush.sh "My commit message"  # Uses provided commit message
