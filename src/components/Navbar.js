87280067e6fb691ca76f4ba4e910387a1e512bd2
abdurafeyf <abdurafeyf@gmail.com>
2023-05-01 21:41:56 +0500

    made use of the daily routing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,7 +18,7 @@ export default function Navbar() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">Today</Link>
+                <Link className="nav-link active" aria-current="page" to="/daily">Today</Link>
               </li>
               <li className="nav-item">
                 <Link className="nav-link active" aria-current="page" to="/weekly">This Week</Link>
