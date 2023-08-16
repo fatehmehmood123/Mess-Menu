c42a7d64f47fa08fe58a5b9661cc8c5254aacf13
Muhammad Fateh Mehmood <fatehmehmood123@gmail.com>
2023-08-16 11:06:02 +0500

    Horizontal scrollablity added

--- src/components/WeekContainer.js
+++ src/components/WeekContainer.js
@@ -11,62 +11,70 @@ export default function WeekContainer(props) {
   }, []);
 
   return (
-    <div id="weekContainer" className="container my-3">
-      <h3>This Week Menu</h3>
+    <div class="scroll-container">
+      <div class="scroll-content">
+        <div id="weekContainer" className="container my-3">
+          <h3>This Week Menu</h3>
 
-      <div className={`table-container ${isActive ? "active" : ""}`}>
-        <table className="container my-4 table table-hover shadow p-3 mb-5 bg-body-tertiary rounded">
-          <tbody>
-            <tr className="table-active">
-              <th>Day</th>
-              <th>Breakfast</th>
-              <th>Lunch</th>
-              <th>Dinner</th>
-            </tr>
-            <tr>
-              <td>Monday</td>
-              <td>{props.monBreakfast}</td>
-              <td>{props.monLunch}</td>
-              <td>{props.monDinner}</td>
-            </tr>
-            <tr>
-              <td>Tuesday</td>
-              <td>{props.tueBreakfast}</td>
-              <td>{props.tueLunch}</td>
-              <td>{props.tueDinner}</td>
-            </tr>
-            <tr>
-              <td>Wednesday</td>
-              <td>{props.wedBreakfast}</td>
-              <td>{props.wedLunch} </td>
-              <td>{props.wedDinner}</td>
-            </tr>
-            <tr>
-              <td>Thursday</td>
-              <td>{props.thursBreakfast}</td>
-              <td>{props.thursLunch}</td>
-              <td>{props.thursDinner}</td>
-            </tr>
-            <tr>
-              <td>Friday</td>
-              <td>{props.friBreakfast}</td>
-              <td>{props.friLunch} </td>
-              <td>{props.friDinner}</td>
-            </tr>
-            <tr>
-              <td>Saturday</td>
-              <td>{props.satBreakfast}</td>
-              <td>{props.satLunch}</td>
-              <td>{props.satDinner}</td>
-            </tr>
-            <tr>
-              <td>Sunday</td>
-              <td>{props.sunBreakfast}</td>
-              <td>{props.sunLunch}</td>
-              <td>{props.sunDinner}</td>
-            </tr>
-          </tbody>
-        </table>
+          <div
+            className={`table-container ${
+              isActive ? "active" : ""
+            } table-responsive`}
+          >
+            <table className="container my-4 table table-hover shadow p-3 mb-5 bg-body-tertiary rounded">
+              <tbody>
+                <tr className="table-active">
+                  <th>Day</th>
+                  <th>Breakfast</th>
+                  <th>Lunch</th>
+                  <th>Dinner</th>
+                </tr>
+                <tr>
+                  <td>Monday</td>
+                  <td>{props.monBreakfast}</td>
+                  <td>{props.monLunch}</td>
+                  <td>{props.monDinner}</td>
+                </tr>
+                <tr>
+                  <td>Tuesday</td>
+                  <td>{props.tueBreakfast}</td>
+                  <td>{props.tueLunch}</td>
+                  <td>{props.tueDinner}</td>
+                </tr>
+                <tr>
+                  <td>Wednesday</td>
+                  <td>{props.wedBreakfast}</td>
+                  <td>{props.wedLunch} </td>
+                  <td>{props.wedDinner}</td>
+                </tr>
+                <tr>
+                  <td>Thursday</td>
+                  <td>{props.thursBreakfast}</td>
+                  <td>{props.thursLunch}</td>
+                  <td>{props.thursDinner}</td>
+                </tr>
+                <tr>
+                  <td>Friday</td>
+                  <td>{props.friBreakfast}</td>
+                  <td>{props.friLunch} </td>
+                  <td>{props.friDinner}</td>
+                </tr>
+                <tr>
+                  <td>Saturday</td>
+                  <td>{props.satBreakfast}</td>
+                  <td>{props.satLunch}</td>
+                  <td>{props.satDinner}</td>
+                </tr>
+                <tr>
+                  <td>Sunday</td>
+                  <td>{props.sunBreakfast}</td>
+                  <td>{props.sunLunch}</td>
+                  <td>{props.sunDinner}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   );
