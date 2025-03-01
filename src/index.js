25a4eed733fdc7413cb0842567ef7d81a654108b
Muhammad Fateh Mehmood <fatehmehmood123@gmail.com>
2025-03-01 21:13:35 +0500

    Theme Context Removed

--- src/index.js
+++ src/index.js
@@ -6,16 +6,14 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import { ThemeProvider } from "./context/ThemeContext";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <React.StrictMode>
       <Provider store={store}>
-        <ThemeProvider>
           <App />
-        </ThemeProvider>
       </Provider>
     </React.StrictMode>
   </BrowserRouter>
