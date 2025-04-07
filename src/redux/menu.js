0a5205f2d1aee8a69204bb85c2581ffe9ae07550
Rafey <abdurafeyf@gmail.com>
2025-04-07 10:03:40 +0500

    Update menu.js

--- src/redux/menu.js
+++ src/redux/menu.js
@@ -94,57 +94,57 @@ const menuSlice = createSlice({
   ],
   reducers: {
     weekChange: (state) => {
-      // state[0].Breakfast = "Fried Egg + Paratha";
-      // state[0].Lunch = "Aluu Palak";
-      // state[0].Dinner = "Beef Shami + Chana Daal + Chatni";
-      // state[1].Breakfast = "Naan Channa";
-      // state[1].Lunch = "Kari Pakora + Rice";
-      // state[1].Dinner = "Channa Pulao";
-      // state[2].Breakfast = "Paratha + Omelette";
-      // state[2].Lunch = "Daal Moong + Salad";
-      // state[2].Dinner = "Chicken Karahi + Kheer";
-      // state[3].Breakfast = "Bread Butter Jam";
-      // state[3].Lunch = "Aluu Matar Gajar";
-      // state[3].Dinner = "Biryani + Cold Drinks";
-      // state[4].Breakfast = "Aalu Anda";
-      // state[4].Lunch = "Daal Chawal (Black)";
-      // state[4].Dinner = "Chicken Daleem + Naan";
-      // state[5].Breakfast = "Aluu paratha";
-      // state[5].Lunch = "Alu Gobi";
-      // state[5].Dinner = "Chicken Pulao + Raita";
-      // state[6].Breakfast = "Halwa Poori Channa";
-      // state[6].Lunch = "Red Bean";
-      // state[6].Dinner = "Beef Chappal Kabab + Mix Daal + Raita";
+      state[0].Breakfast = "Fried Egg + Paratha";
+      state[0].Lunch = "Aluu Palak";
+      state[0].Dinner = "Beef Shami + Chana Daal + Chatni";
+      state[1].Breakfast = "Naan Channa";
+      state[1].Lunch = "Kari Pakora + Rice";
+      state[1].Dinner = "Channa Pulao";
+      state[2].Breakfast = "Paratha + Omelette";
+      state[2].Lunch = "Daal Moong + Salad";
+      state[2].Dinner = "Chicken Karahi + Kheer";
+      state[3].Breakfast = "Bread Butter Jam";
+      state[3].Lunch = "Aluu Matar Gajar";
+      state[3].Dinner = "Biryani + Cold Drinks";
+      state[4].Breakfast = "Aalu Anda";
+      state[4].Lunch = "Daal Chawal (Black)";
+      state[4].Dinner = "Chicken Daleem + Naan";
+      state[5].Breakfast = "Aluu paratha";
+      state[5].Lunch = "Alu Gobi";
+      state[5].Dinner = "Chicken Pulao + Raita";
+      state[6].Breakfast = "Halwa Poori Channa";
+      state[6].Lunch = "Red Bean";
+      state[6].Dinner = "Beef Chappal Kabab + Mix Daal + Raita";
 
 
 //Raaamzan
-      state[0].Breakfast = "Murgh Channay";
-      state[0].Lunch = "Chicken Veg Roll, Pakora Fries, Jaame-Sheerin";
-      state[0].Dinner = "Chicken Manchurian, Chinese Rice";
+      // state[0].Breakfast = "Murgh Channay";
+      // state[0].Lunch = "Chicken Veg Roll, Pakora Fries, Jaame-Sheerin";
+      // state[0].Dinner = "Chicken Manchurian, Chinese Rice";
 
-      state[1].Breakfast = "Aloo Anda Onion";
-      state[1].Lunch = "Aloo Samosa, Mix Pakora, Tang Lemon";
-      state[1].Dinner = "Chicken Curry";
+      // state[1].Breakfast = "Aloo Anda Onion";
+      // state[1].Lunch = "Aloo Samosa, Mix Pakora, Tang Lemon";
+      // state[1].Dinner = "Chicken Curry";
 
-      state[2].Breakfast = "White Channa";
-      state[2].Lunch = "Dahi Bhalay, Mix pakora, Jaame-Sheerin";
-      state[2].Dinner = "Chicken Kofta";
+      // state[2].Breakfast = "White Channa";
+      // state[2].Lunch = "Dahi Bhalay, Mix pakora, Jaame-Sheerin";
+      // state[2].Dinner = "Chicken Kofta";
 
-      state[3].Breakfast = "Aloo Keema";
-      state[3].Lunch = "Chicken Veg Roll, Pakora Fries, Tang Orange";
-      state[3].Dinner = "Biryani , Cold Drinks";
+      // state[3].Breakfast = "Aloo Keema";
+      // state[3].Lunch = "Chicken Veg Roll, Pakora Fries, Tang Orange";
+      // state[3].Dinner = "Biryani , Cold Drinks";
 
-      state[4].Breakfast = "Omelette";
-      state[4].Lunch = "Channa Chaat, Mix Pakora, Rooh Afza";
-      state[4].Dinner = "Chicken Daleem, Naan";
+      // state[4].Breakfast = "Omelette";
+      // state[4].Lunch = "Channa Chaat, Mix Pakora, Rooh Afza";
+      // state[4].Dinner = "Chicken Daleem, Naan";
 
-      state[5].Breakfast = "Chicken Curry";
-      state[5].Lunch = "Chicken Veg Samosa, Mix Pakora, Tang Orange";
-      state[5].Dinner = "Chicken Nihari, Naan";
+      // state[5].Breakfast = "Chicken Curry";
+      // state[5].Lunch = "Chicken Veg Samosa, Mix Pakora, Tang Orange";
+      // state[5].Dinner = "Chicken Nihari, Naan";
 
-      state[6].Breakfast = "Aloo Anda Curry";
-      state[6].Lunch = "Lobia & Channa Chaat, Mix Pakora, Rooh Afza";
-      state[6].Dinner = "Chicken Pulao";
+      // state[6].Breakfast = "Aloo Anda Curry";
+      // state[6].Lunch = "Lobia & Channa Chaat, Mix Pakora, Rooh Afza";
+      // state[6].Dinner = "Chicken Pulao";
     },
   },
 });
